000a4f0f3bfbc3cad2628cc9ccdd45091e67edef
SahutoL <96646448+SahutoL@users.noreply.github.com>
2024-05-26 16:21:37 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,3 @@ from app.routers import reading_data
 app = FastAPI()
 
 app.include_router(reading_data.router, prefix="/api/v1")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
