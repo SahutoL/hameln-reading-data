67073c49d4b330bc31c9c78c8669741e97aece3c
SahutoL <96646448+SahutoL@users.noreply.github.com>
2024-07-02 22:59:46 +0900

    services.py を更新

--- app/services.py
+++ app/services.py
@@ -35,7 +35,7 @@ def login_and_get_reading_data(userId: str, password: str):
             chapter_count = info[1].get_text().replace("\n","").replace(" ","").replace("\t","").replace(",","")
             word_count = info[2].get_text().replace("\n","").replace(" ","").replace("\t","").replace(",","")
             daily_table = soup.find('li', class_='id6')
-            print(daily_tabel)
+            print(daily_table)
             reading_data.append({
                 "year": year,
                 "month": month,
